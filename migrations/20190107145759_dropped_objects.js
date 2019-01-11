bdb4f0e398693f3d8070bc2fc81d2260ce2b41a1
ryanholly3 <ryanholly3@gmail.com>
2019-01-11 10:47:14 -0700

    modified knex migration dropped_object lat long parameters

--- migrations/20190107145759_dropped_objects.js
+++ migrations/20190107145759_dropped_objects.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('dropped_object', function(table) {
         table.increments();
-        table.float('latitude');
-        table.float('longitude');
+        table.float('latitude', 14, 10).notNullable();
+        table.float('longitude', 14, 10).notNullable();
         table.integer('object_id').references('object.id').unsigned().onDelete('cascade');
     });
 };
